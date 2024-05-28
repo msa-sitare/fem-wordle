9cd93dba339c4ab716ff053297bfd90de73dceb2
msa-sitare <atessude@hotmail.com>
2024-05-28 18:18:46 +0200

    add commit addletter backspace functions

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 const letters = document.querySelectorAll(".box");
-console.log(letters);
 const ANSWER_LENGTH = 5;
+const ROUNDS = 6;
+const winner = document.querySelector(".winner");
+const loser = document.querySelector(".loser");
+const loserMessage = document.getElementById("loser-message");
 
 async function init() {
   let currentRow = 0;
   let currentGuess = "";
+  let isLoading = true;
+
+  const res = await fetch("https://words.dev-apis.com/word-of-the-day");
+  const resObj = await res.json();
+  const word = resObj.word.toUpperCase();
+  const wordParts = word.split("");
+  let done = false;
+  isLoading = false;
 
   function addLetter(letter) {
     if (currentGuess.length < ANSWER_LENGTH) {
@@ -23,7 +34,37 @@ async function init() {
       return;
     }
 
+
+    const guessParts = currentGuess.split("");
+    const map = makeMap(wordParts);
+
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      if (guessParts[i] === wordParts[i]) {
+        //guess the correct word in place
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("correct");
+        map[guessParts[i]]--;
+      } else if (wordParts.includes(guessParts[i]) && map[guessParts[i]] > 0) {
+        //guess the correct word in wrong place
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("close");
+        map[guessParts[i]]--;
+      } else {
+        //guess wrong
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("wrong");
+      }
+    }
+
     currentRow++;
+
+    if (currentGuess === word) {
+      winner.classList.toggle("hidden");
+      done = true;
+      return;
+    } else if (currentRow === ROUNDS) {
+      loserMessage.innerText = `You lose, the word was ${word}`;
+      loser.classList.toggle("hidden");
+      done = true;
+    }
+
     currentGuess = "";
   }
 
@@ -33,8 +74,12 @@ async function init() {
   }
 
   document.addEventListener("keydown", function handleKeyPress(event) {
+    if (done || isLoading) {
+      // do nothing
+      return;
+    }
+
     const action = event.key;
-    console.log(action);
 
     if (action === "Enter") {
       commit();
@@ -50,6 +95,21 @@ async function init() {
 
 function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
+}
+
+function makeMap(array) {
+  //count how many same letters are there
+  const obj = {};
+  for (let i = 0; i < array.length; i++) {
+    const letter = array[i];
+    if (obj[letter]) {
+      obj[letter]++;
+    } else {
+      obj[letter] = 1;
+    }
+  }
+
+  return obj;
 }
 
 init();
